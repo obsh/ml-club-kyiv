b0e0c15d7b1630af146e0c6600ef0ddb4aadf57b
oleksandr.bushkovskyi <bushkovsky@gmail.com>
2019-12-01 02:35:51 +0200

    Initial commit

---
+++ meetup_01/src/spider.py
@@ -0,0 +1,22 @@
+import scrapy
+
+
+class LunSpider(scrapy.Spider):
+    name = 'lun_spider'
+    base_url = 'https://flatfy.lun.ua/uk/%D0%BF%D1%80%D0%BE%D0%B4%D0%B0%D0%B6-%D0%BA%D0%B2%D0%B0%D1%80%D1%82%D0%B8%D1%80-%D0%BA%D0%B8%D1%97%D0%B2'
+    start_urls = [base_url + '?page=%s' % page for page in range(1, 101)]
+
+    def parse(self, response):
+        for article in response.css('article.jss140'):
+            record = {
+                'street': article.css('a.jss159 ::text').get(),
+                'district': article.css('a.jss166 ::text').getall(),
+                'price': article.css('div.jss171 ::text').get(),
+                'short_description': article.css('div.jss179 ::text').get(),
+                'image': article.css('picture > source::attr(srcset)').get(),
+                'attributes': [],
+            }
+            for ul in article.css('ul.jss174'):
+                record['attributes'].append(ul.css('li.jss175 ::text').getall())
+
+            yield record
