6e34c21905b7edec55cd914fa3b60f1d5525d4b8
oleksandr.bushkovskyi <bushkovsky@gmail.com>
2019-12-01 02:50:44 +0200

    change start_urls

--- meetup_01/src/spider.py
+++ meetup_01/src/spider.py
@@ -3,8 +3,7 @@ import scrapy
 
 class LunSpider(scrapy.Spider):
     name = 'lun_spider'
-    base_url = 'https://flatfy.lun.ua/uk/%D0%BF%D1%80%D0%BE%D0%B4%D0%B0%D0%B6-%D0%BA%D0%B2%D0%B0%D1%80%D1%82%D0%B8%D1%80-%D0%BA%D0%B8%D1%97%D0%B2'
-    start_urls = [base_url + '?page=%s' % page for page in range(1, 101)]
+    start_urls = ['https://flatfy.lun.ua/uk/%D0%BF%D1%80%D0%BE%D0%B4%D0%B0%D0%B6-%D0%BA%D0%B2%D0%B0%D1%80%D1%82%D0%B8%D1%80-%D0%BA%D0%B8%D1%97%D0%B2?page={}'.format(page) for page in range(1, 101)]
 
     def parse(self, response):
         for article in response.css('article.jss140'):
